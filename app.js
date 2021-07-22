b49d25b7fcf89768ceace095313a387a8c84ff42
David <wideoutinfootball@gmail.com>
2021-07-22 11:15:28 -0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Pokemon shananagans is afoot'));
+app.get('/', (req, res) => res.send('I like what i like, cant help it'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
